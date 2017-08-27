8e0a40f36c8dbe7dd99ebdf1c4a929ce9516c2f8
Gus Pass <passosgu@sempreit.com.br>
2017-08-26 22:45:17 -0300

    xml file defined and loader script struct created

---
+++ Waifu-GO/Assets/Scripts/DialogueSystem/LoadDialogue.cs
@@ -0,0 +1,34 @@
+﻿///
+/// @author GusPassos
+/// 
+
+namespace WaifuGO.DialogueSystem
+{
+    /// <summary>
+    /// Reads a .xml file and parse its content into a DialogueNode struct
+    /// </summary>
+    public class LoadDialogue
+    {   
+        
+    }
+    
+    /// <summary>
+    /// A dialogue node that defines the current state of the conversation
+    /// </summary>
+    public struct DialogueNode
+    {
+        public string       girlLine;
+        public Option[]     options;
+    }
+
+    /// <summary>
+    /// A dialogue option that takes to another dialogue node
+    /// </summary>
+    public struct Option
+    {
+        // may have some other vars that defines if this was a good choice or not
+        public string   optionLine;
+        public int      nextNode;
+    }
+
+}
